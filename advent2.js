b9f8a73ecf303e42ae05728d5cdb19eb75f6b3b8
erathe <eirik.rathe@gmail.com>
2017-12-02 16:34:04 +0100

    trimmed the input

--- advent2.js
+++ advent2.js
@@ -1,8 +1,8 @@
 const R = require('ramda');
 const fs = require('fs');
-const input = fs.readFileSync('./input2', 'utf8');
+const input = fs.readFileSync('./input2').toString().trim();
 
-const isDivisable = x => y => x !== y && ( x % y === 0 || y % x === 0); 
+const isDivisable = x => y => x !== y && ( x % y === 0 || y % x === 0);
 const diff = (x,y) => x-y;
 const splitAndConvertToIntAndSort = R.compose((R.sort(diff)), R.map(Number), R.split('\t'));
 const sortedRow = R.compose(R.map(splitAndConvertToIntAndSort), R.split('\n'));
@@ -12,10 +12,8 @@ const result = sortedRow(input).reduce(( acc, curr ) => {
 },0);
 
 const result2 = sortedRow(input).reduce(( acc, curr ) => {
-    if (curr.length > 1){
-        const div = curr => (([x, y]) => y / x)(curr.filter(x => curr.some(isDivisable(x))));
-        return acc + div(curr);
-    } else {return acc + 0} // bug in file read? a zero is added at the end of the split for some reason
+    const div = curr => (([x, y]) => y / x)(curr.filter(x => curr.some(isDivisable(x))));
+    return acc + div(curr);
 },0);
 
 console.log(result, result2);
