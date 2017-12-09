be8c3ad1d06cc193c09a8ef0732d31de7bd3c22d
erathe <eirik.rathe@gmail.com>
2017-12-09 13:35:13 +0100

    part 2

--- advent7.js
+++ advent7.js
@@ -1,8 +1,24 @@
 const fs = require('fs');
 const input = fs.readFileSync('./input7').toString().trim().split('\n');
 
-let nodes = {};
-let holding = {};
+let [nodes, holding, root] = [{}, {}, ''];
+
+const dfs = (rooter, holding, nodes) => {
+    let [exp, weight] = [0, nodes[rooter]];
+    for (child of holding[rooter]){
+        //Holy scoping rules
+        const ch = child;
+        let w = dfs(ch, holding, nodes);
+        weight += w;
+        if(exp === 0) exp = w;
+        else if(exp != w){
+            const bad = nodes[ch];
+            console.log('expcted child', ch, 'to have weight', nodes[ch] += (exp-w), 'has weight', bad);
+        }
+    }
+    return weight;
+}
+
 const a = input.map(row => {
     let rowCont = row.split(' ');
     nodes[rowCont[0]] = Number((rowCont[1].replace(/\(|\)/g, '')));
@@ -10,7 +26,7 @@ const a = input.map(row => {
     holding[rowCont[0]] = hold;
 });
 
-const result = Object.keys(nodes).forEach(node => {
-    if (!Object.values(holding).some(r => r.includes(node))) console.log(node);
+const result = Object.keys(holding).forEach(node => {
+    if (!Object.values(holding).some(r => r.includes(node))) root = node; 
 })
-
+console.log(root, dfs(root, holding, nodes));
