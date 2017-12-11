bf5c526ccd6138eace82e887e0efa9e2e4022e41
erathe <eirik.rathe@gmail.com>
2017-12-11 07:42:20 +0100

    solution day 11

---
+++ advent11.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+console.time("run");
+const input = fs.readFileSync('./input11').toString('utf-8').trim().split(',');
+
+let[x, y, z] = [0,0,0];
+let max = 0;
+for (dir of input){
+    if(dir === 'ne') x+=1;
+    else if(dir === 's') y-=1;
+    else if(dir === 'sw') x-=1;
+    else if(dir === 'n') y+=1;
+    else if(dir === 'se'){
+        x += 1;
+        y -= 1;
+    }
+    else if(dir === 'nw'){
+        x -= 1;
+        y += 1;
+    }
+    distance = ((Math.abs(x) + Math.abs(y) + Math.abs(x+y))/2);
+    if(distance > max) max = distance;
+}
+
+
+console.log(max, distance);
+console.timeEnd("run");
