20cb9978abc13deceec36d756264955f93c5ff58
erathe <eirik.rathe@gmail.com>
2017-12-16 11:08:12 +0100

    removed unused vars and functions

--- advent15.js
+++ advent15.js
@@ -2,11 +2,8 @@
 const fs = require('fs');
 console.time("run");
 
-let [startAaa, startBaa, i, pairs] = [699, 124, 0, 0];
+let [i, pairs] = [0, 0];
 const [divOp, genA, genB] = [2147483647, 16807, 48271];
-
-const convertToBinary = num => num.toString(2);
-const matchPairs = (valA, valB) => valA.slice(-16) === valB.slice(-16);
 
 function* genPairs(){
   let [startA, startB] = [699, 124]
